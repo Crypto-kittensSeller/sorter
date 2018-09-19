ccd7b192de865d9cd4e208c54ddb6a969b2d576c
Crypto-kittensSeller <Nikolay.Shaveiko@gmail.com>
2018-09-19 15:22:13 +0300

    sorter beta version

--- src/index.js
+++ src/index.js
@@ -1,26 +1,35 @@
 class Sorter {
   constructor() {
-    // your implementation
+   this.arr = []; 
+   this.newarr =[];
   }
-
+  
   add(element) {
-    // your implementation
+    return this.arr.push(element);
   }
 
   at(index) {
-    // your implementation
+    return this.arr[index];
   }
 
   get length() {
+    return this.arr.length;
     // your implementation
   }
 
   toArray() {
-    // your implementation
+    return this.arr;
   }
 
   sort(indices) {
-    // your implementation
+    
+    indices.sort(function(a,b){return a - b});
+    this.newarr = this.arr.slice(indices[0],indices[0]+1).concat(this.arr.slice(indices[1],indices[1]+1),this.arr.slice(indices[2],indices[2]+1));
+    this.newarr.sort(function(a,b){return a - b});
+    for (var i = 0; i<indices.length; i++){
+    this.arr[indices[i]] = this.newarr[i];
+    }
+  
   }
 
   setComparator(compareFunction) {
